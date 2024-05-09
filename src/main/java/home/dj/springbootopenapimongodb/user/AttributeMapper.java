babb0a48895117ab6d7c36b2bcc09503eafa20e6
douglasjohnson <douglas.johnson4@gmail.com>
2024-05-09 10:18:09 +0100

    Rename to top trumps

--- src/main/java/home/dj/springbootopenapimongodb/user/AttributeMapper.java
+++
@@ -1,9 +0,0 @@
-package home.dj.springbootopenapimongodb.user;
-
-import home.dj.model.Attribute;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface AttributeMapper {
-    AttributeEntity dtoToEntity(Attribute attribute);
-}
--- src/main/java/home/dj/springbootopenapimongodb/user/AttributeTypeMapper.java
+++
@@ -1,9 +0,0 @@
-package home.dj.springbootopenapimongodb.user;
-
-import home.dj.model.AttributeType;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface AttributeTypeMapper {
-    AttributeTypeEntity dtoToEntity(AttributeType attributeType);
-}
