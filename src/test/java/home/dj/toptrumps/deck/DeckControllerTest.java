c4c90e0315b28a169a041578fe74246b8eef022d
Douglas Johnson <douglas.johnson4@gmail.com>
2024-06-03 22:04:44 +0100

    Exclude security filters from controller test

--- src/test/java/home/dj/toptrumps/deck/DeckControllerTest.java
+++ src/test/java/home/dj/toptrumps/deck/DeckControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(DeckController.class)
 @ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc(addFilters = false)
 class DeckControllerTest {
 
     @Autowired
