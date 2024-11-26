60179899660ce4333f0dae5b5383abdbdca0fc02
Douglas Johnson <douglas.johnson4@gmail.com>
2024-11-26 20:08:23 +0000

    return error on image upload failure

--- src/main/java/home/dj/toptrumps/image/ImageController.java
+++ src/main/java/home/dj/toptrumps/image/ImageController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +16,10 @@ public class ImageController implements ImagesApi {
     private final ImageService imageService;
     @Override
     public ResponseEntity<UploadImage201Response> uploadImage(MultipartFile file) {
-        return new ResponseEntity<>(new UploadImage201Response().url(imageService.upload(file)), CREATED);
+        String url = imageService.upload(file);
+        if (url != null) {
+            return new ResponseEntity<>(new UploadImage201Response().url(url), CREATED);
+        }
+        return new ResponseEntity<>(INTERNAL_SERVER_ERROR);
     }
 }
